d4b7a0a45c7e87957650d53f2aff50a990ff910a
yeongguo <yeongguo@tencent.com>
2019-07-10 15:40:08 +0800

    添加web view的demo

---
+++ lib/webview/webview_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewPage extends StatefulWidget {
+
+  @override
+  _WebViewPageState createState() => _WebViewPageState();
+}
+
+/// WebView 使用实例
+/// Dart 层的webView通过 javascriptChannels 与 js 桥接通信
+
+class _WebViewPageState extends State<WebViewPage> {
+  final String _initialUrl = 'http://www.baidu.com';
+
+  WebViewController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WebView'),
+      ),
+      body: Container(
+        color: Colors.red,
+        child: Stack(
+          children: <Widget>[
+            WebView(
+              initialUrl: _initialUrl,
+              onWebViewCreated: (controller){
+                _controller = controller;
+              },
+              onPageFinished: (url){
+                print('on page finished:$url');
+              },
+              debuggingEnabled: true,
+              javascriptMode: JavascriptMode.unrestricted,
+              javascriptChannels: <JavascriptChannel>[JavascriptChannel(name: 'demoBridge', onMessageReceived: null)].toSet(),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
