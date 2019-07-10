1c11412a1cb9c4835ce8867f315fb6aa5d25e3f9
yeongguo <yeongguo@tencent.com>
2019-07-10 11:00:10 +0800

    添加测试用例

---
+++ test/get_rect_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/utils/render_box_util.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+  test('test getRect method',(){
+    Builder(builder: (context){
+      print(getRect(context));
+    });
+  });
+}
