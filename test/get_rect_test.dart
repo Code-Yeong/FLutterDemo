6d5a6ecedc311e6ea06cd3b67a66384a90593193
yeongguo <yeongguo@tencent.com>
2019-08-02 10:39:01 +0800

    格式优化

--- test/get_rect_test.dart
+++ test/get_rect_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_demo/utils/render_box_util.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main(){
-  test('test getRect method',(){
+  test('test getRect methods',(){
     Builder(builder: (context){
       print(getRect(context));
       return null;
